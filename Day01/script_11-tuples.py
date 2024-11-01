f81dd8c20dbe2c235f10f762b6ddf5c7ec688b8c
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-11-01 22:10:31 +0600

    Python Fundamentals
    Python Fundamentals

---
+++ Day01/script_11-tuples.py
@@ -0,0 +1,7 @@
+# 11. Tuples
+"""Tuples are immutable collections of items.
+    Syntax:
+        tuple_name = (item1, item2, ...)
+"""
+coordinates = (10.0, 20.0)
+print("X Coordinate:", coordinates[0])
