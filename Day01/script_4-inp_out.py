1df018187d43d523ebaac7fea38f5facdb5b0e5a
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-11-01 22:05:35 +0600

    Python fundamentals
    Python fundamentals

---
+++ Day01/script_4-inp_out.py
@@ -0,0 +1,12 @@
+# 4. Basic I/O Operations
+"""Input and Output operations allow interaction with users.
+    Syntax:
+        input - var_name = input("message") // take input from user
+        and save it into "var_name"
+        output - print("welcome " + var_name) // expected output:
+        "welcome (users inputed satement)"
+"""
+# Input
+user_name = input("Enter your name: ")
+# Output
+print("Salam, " + user_name + "!")
---
+++ Day01/script_5-bolean.py
@@ -0,0 +1,8 @@
+# 5. Boolean Values
+"""Booleans represent True or False values.
+    Syntax:
+        var_name = True
+        var_name = False
+"""
+is_student = True
+is_graduated = False
