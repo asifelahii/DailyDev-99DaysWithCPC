3612e45e2ab63d6369ef2d7a58796b55ef404494
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-11-02 22:37:40 +0600

    Python Fundamentals
    Python Fundamentals

---
+++ Day01/script_12-dictionaries.py
@@ -0,0 +1,10 @@
+# 12. Dictionaries
+"""Dictionaries store data as key-value pairs.
+    Syntax:
+        dict_name = {"key1": value1, "key2": value2, ...}
+"""
+person = {
+    "name": "Alice",
+    "age": 25
+}
+print("Name:", person["name"])
