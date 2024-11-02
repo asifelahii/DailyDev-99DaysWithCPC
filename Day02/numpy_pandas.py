92b5eea6a5823b7d3135e9ecd47484c46bb299f7
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-11-02 23:38:47 +0600

    Pandas as pd
    Pandas as pd

--- Day02/numpy_pandas.py
+++ Day02/numpy_pandas.py
@@ -1,3 +1,21 @@
 import numpy as numpy
 import pandas as pd
 
+# creating a dictionary
+dic = {
+    "Name": ["Asif", "Abdullah", "A.Rahman", "A.Rahim"],
+    "Age": [20, 22, 19, 18],
+    "City": ["Dhaka", "Rajshahi", "Madina", "Cairo"]
+}
+
+# creating a DataFrame from the dictionary
+df = pd.DataFrame(dic)
+print(df) 
+
+# Export the dataframe
+# Save to CSV
+df.to_csv('dic_with_indices.csv')
+df.to_csv('dic_without_indices.csv', index = False)
+#Save to Excel
+df.to_excel('dic_with_indices.xlsc')
+df.to_excel('dic_without_indices.xlsc', index = False)
