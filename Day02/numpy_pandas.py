abf7f45b6c1b5af3b1416af2446c42fc66ac118f
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-11-03 23:38:53 +0600

    Numpy and Pandas
    numpy as np and pandas as pd

--- Day02/numpy_pandas.py
+++ Day02/numpy_pandas.py
@@ -16,6 +16,21 @@ print(df)
 # Save to CSV
 df.to_csv('dic_with_indices.csv')
 df.to_csv('dic_without_indices.csv', index = False)
-#Save to Excel
-df.to_excel('dic_with_indices.xlsc')
-df.to_excel('dic_without_indices.xlsc', index = False)
+#Save to Excel ('openpyxl' must to be installed in our environment to export data frame to excel)
+df.to_excel('dic_with_indices.xlsx')
+df.to_excel('dic_without_indices.xlsx', index = False)
+
+"""display n columns from head / tail
+    Syntax:
+        df.head(n)
+        df.tail(n)
+"""
+print(df.head(2))
+print()
+print(df.tail(2))
+
+"""To analyze numerical columns-
+    Syntax:
+        df.describe()
+"""
+print(df.describe())
